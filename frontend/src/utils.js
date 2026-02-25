1e1f973763f93d191fd31f7031e66c1c6288de56
mikebams1194-droid <mikebams1194@gmail.com>
2026-02-25 11:21:41 +0100

    Update API_BASE_URL to verified Choreo path

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "/choreo-apis/chooseyourownadventureaig/backend/v1/api"
+export const API_BASE_URL = "https://ff2e0793-8fbb-4792-be41-a50c50764df3-dev.e1-us-east-azure.choreoapis.dev/chooseyourownadventureaig/backend/v1.0";
